26961e31dd6427ce506fadea7d7fdd339a36d8cb
austin2011 <amoigus42069@proton.me>
2024-03-30 13:01:05 -0400

    5eu6tygju54y6t35yh6trujdhuner564y6r5tyugte675

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public GameObject door;
+    public ParticleSystem pickup;
+
+    void Start()
+    {
+        pickup.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin")) OnCoinTriggerEnter(other);
+    }
+
+    void OnCoinTriggerEnter (Collider other)
+    {
+        Destroy(door);
+        Destroy(other.gameObject);
+    }
+}
